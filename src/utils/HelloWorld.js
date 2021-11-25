ed69455ecd99e1f935812cc21ad574febc766f17
tobydustin <tobydustin@tobydustins-MacBook-Pro.local>
2021-11-25 10:51:13 +0000

    added ngnix routing

--- src/utils/HelloWorld.js
+++ src/utils/HelloWorld.js
@@ -1,5 +1,5 @@
 export const fetchFromBFF = new Promise((resolve,reject)=>{
-    fetch('http://localhost:8000/')
+    fetch('http://localhost/bff')
     .then(response => response.json())
     .then(data => resolve(data))
     .catch(_ => resolve({message:'WHOOPS! not connected to the BFF.',load: false}))
